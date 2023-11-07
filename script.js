ed070818e4c1dbb52f39cba3a0bffda8f2a8d0c4
Hong <hoang.bui49@gmail.com>
2023-11-06 18:26:04 -0800

    Added comments to explain script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+//Create array to hold books
 const myLibrary = []
 
+//Constructor for books
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -7,20 +9,31 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+
 function addBookToLibrary() {
+    //Sets book variables based on input elements from form in HTML
     let title = document.querySelector('#title').value;
     let author = document.querySelector('#author').value;
     let pages = document.querySelector('#pages').value;
     let read = document.querySelector('#read').value;
+
+    //Passes variables to Book constructor to create 'nextbook'.
     let nextBook = new Book(title, author, pages, read);
+
+    //Pushes new 'nextBook' into library array
     myLibrary.push(nextBook)
-    console.table(myLibrary);
+
+    //Runs function that adds book in array as HTML element
     addToBookcase();
 }
 
 function addToBookcase() {
     let bookcase = document.querySelector('.bookcase');
+
+    //Clears html so that adding a book to the existing and adding the array to HTML with existing books  wont duplicate books that were in the array originally
     bookcase.innerHTML='';
+
+    //Loops through every book in library array and creates html elements from currentBook info
     for (let i = 0; i < myLibrary.length; i++) {
         let currentBook = myLibrary[i];
         let createBook = document.createElement('div');
@@ -38,12 +51,14 @@ function addToBookcase() {
     }
 }
 
+//Add book button shows the 'dialog' element that is normally hidden which allows for the addition of books
 let newBook = document.querySelector('.addBook');
 newBook.addEventListener('click', () => {
     let bookForm = document.querySelector('.bookFormDia');
     bookForm.showModal();
 })
 
+//Submitting logs the values in the inputs of the form, runs the addBookToLibrary function to create books from these values, and closes the form dialog
 let submit = document.querySelector('#submit');
 submit.addEventListener('click', (event) => {
     event.preventDefault();
@@ -52,6 +67,7 @@ submit.addEventListener('click', (event) => {
     bookForm.close();
 })
 
+//Clickingn the delete button on a specific book (item in the array), deletes that element from the array and re-creates HTMl for bookcase without that element.
 function removeBook(index) {
     myLibrary.splice(index, 1);
     addToBookcase();
